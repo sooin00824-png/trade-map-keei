4ab8554f7a3b7fbc0b3db9ba676a264ebcab8a10
sooin00824-png <sooin00824@gmail.com>
2025-10-26 14:57:58 +0900

    Update trade_map_Li-Co.py

--- trade_map_Li-Co.py
+++ trade_map_Li-Co.py
@@ -21,6 +21,8 @@ st.title("🌐 리튬 및 코발트 국제 교역 지도")
 def load_data():
     url = "https://raw.githubusercontent.com/sooinkim/trade-map-keei/main/netwgt_import_monthly.csv"
     data = pd.read_csv(url)
+    for col in ['period', 'cmdcode', 'reporter', 'partner']:
+        data[col] = data[col].astype(str).str.strip()
     data.columns = data.columns.str.lower()
     data['reporter'] = data['reporter'].astype(str).str.strip().str.title()
     data['cmdcode'] = data['cmdcode'].astype(str).str.strip()
@@ -165,6 +167,7 @@ else:
 # ------------------------------
 st.markdown("---")
 st.caption("📊 **Source:** UN COMTRADE Database")
+
 
 
 
